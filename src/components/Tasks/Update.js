1b2832786ca7cbd179cea6dd5e9bbb54e861083a
valcaro87@gmail.com <valcaro87@gmail.com>
2022-04-27 09:50:36 +0800

    added next and previous ids

--- src/components/Tasks/Update.js
+++ src/components/Tasks/Update.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { useMutation, useQuery } from "react-query"
+import { Link } from "react-router-dom"
 import { Form } from "./Form"
 import { getTask, updateTask } from "../../api"
 import { Box, Flex } from "rebass/styled-components"
@@ -10,14 +11,16 @@ export const Update = () => {
   const { id } = useParams()
   const history = useNavigate()
   const { data } = useQuery(["task", { id }], getTask)
+  const [statez, setStatez] = useState(Date.now())
 
   const { mutateAsync } = useMutation(updateTask)
   const onFormSubmit = async (data) => {
     await mutateAsync({ ...data, id })
-    history('/tasks')
+    //setStatez(Date.now())
+    history('/tasks/')
   }
 
-  const [statez, setStatez] = useState(Date.now())
+
   useEffect(() => {
     setStatez(Date.now())
   }, [data])
@@ -32,7 +35,14 @@ export const Update = () => {
 
               <Form key={statez} defaultValues={data?.results} onFormSubmit={onFormSubmit} isLoading={false} btnText="Update" />
             </Box>
+            <Link to={`/tasks/${data?.results.previous_id}`}>
+              <span>Previous</span>
+            </Link>
+            <Link to={`/tasks/${data?.results.next_id}`}>
+              <span>Next</span>
+            </Link>
           </Flex>
+
         </>
       )}
     </>
